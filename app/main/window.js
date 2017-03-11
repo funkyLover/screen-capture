67c3c186cb249a1a5e0b29ce7a636b672f5fe43a
林龙辉 <540886643@qq.com>
2017-03-11 22:34:33 +0800

    + capture.html

--- app/main/window.js
+++ app/main/window.js
@@ -6,25 +6,24 @@ function createWindow () {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize
   const winWidth = 100
   const winHeight = 80
-  let win = new BrowserWindow({
+  let tipsWin = new BrowserWindow({
     width: winWidth,
     height: winHeight,
     x: (width - winWidth) / 2,
     y: 100,
     resizable: false,
-    frame: false
+    frame: false,
+    alwaysOnTop: true
   })
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, '../render/index.html'),
+  tipsWin.loadURL(url.format({
+    pathname: path.join(__dirname, '../render/Tips.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // win.webContents.openDevTools()
-
-  win.on('closed', () => {
-    win = null
+  tipsWin.on('closed', () => {
+    tipsWin = null
   })
 }
 
